92559a10805ba170b4266ef002d925a199fa71b0
Jack <jdcorley@gmail.com>
2019-05-06 18:06:27 -0400

    integrated bootstrap in webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "./application.css";
+import "bootstrap/dist/css/bootstrap.css";
 console.log('Hello World from Webpacker');
